dafc76e31d3af81b904f70259aadba5f32b1c60e
Neel Jasani <Neel@radicalrack.local>
2025-01-08 17:18:27 +0530

    first

--- index.js
+++ index.js
@@ -1,44 +1,20 @@
-const express=require("express");
-const fs=require('fs');
-const users=require("./MOCK_DATA.json");
-const app=express();
-//Middleware
-app.use(express.urlencoded({extended:false}))
-//Routes
+const express = require("express");
+const {connectedMongoDb}=require('./connection')
+const {logReqRes}=require("./middlewares")
+const userRouter=require('./routes/user')
 
-app.get("/users",(req,res)=>{
-    const html=`
-    <ul>
-    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `
-    res.send(html);
-})
-//Rest API
-app.get("/api/users",(req,res)=>{
-    return res.json(users);
-});
+const app = express();
 
-app.route("/api/users/:id").get((req,res)=>{
-    const id=Number(req.params.id);
-    const user=users.find((user) => user.id === id);
-    return res.json(user);
-})
-.patch((req,res)=> {
-    return res.json({status:'pending'})
-})
-.delete((req,res)=> {
-    return res.json({status:'pending'})
-});
 
-app.post("/api/users",(req,res)=>{
-    const body=req.body;
-    users.push({...body,id : users.length + 1});
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-        return res.json({status : 'Success',id : users.length});
-    })
-});
+connectedMongoDb("mongodb://127.0.0.1:27017/youtube-app-1").then(() => console.log("MongoDB Connected"));
 
-app.listen(8000,() => {
-    console.log("server started");    
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use(logReqRes("log.txt"))
+
+app.use("/api/users",userRouter);
+
+app.listen(8000, () => {
+    console.log("server started");
 })
